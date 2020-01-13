d22c04d70b87b0a0f76baf42b6a36d6ea509629a
Johnny <johnnyi.london2019a@gmail.com>
2020-01-13 10:37:04 +0000

    Commit 1

--- src/main/java/numbers/Number.java
+++ src/main/java/numbers/Number.java
@@ -1,4 +1,27 @@
 package numbers;
 
 public class Number {
+    private String phoneNumber;
+
+    public Number(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+
+
+        char[] cArray = phoneNumber.toCharArray();
+        if (cArray.length == 12){
+            setPhoneNumber(phoneNumber);
+        }
+        else
+            setPhoneNumber("Invalid!");
+
+
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/test/java/numbers/NumberTest.java
+++ src/test/java/numbers/NumberTest.java
@@ -1,4 +1,29 @@
 package numbers;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NumberTest {
+    @Test
+    public void takes_a_valid_length_number_and_returns_it() {
+        //arrange
+        String input = "+11010101010";
+        String expectedResult = "+11010101010";
+        Number cut = new Number(input);
+
+        //act
+        String result = cut.getPhoneNumber();
+
+        //assert
+        assertEquals(expectedResult, result);
+
+    }
+
+    @Test
+    public void test2()
+    {
+
+    }
+
+
 }
